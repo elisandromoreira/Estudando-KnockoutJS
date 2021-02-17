18f06aef2dc9350af0a3895d953324a62372d62f
Elisandro Moreira <elisandro.moreira@univille.br>
2021-02-17 14:22:33 -0300

    Add computed observables

--- 02/app.js
+++ 02/app.js
@@ -1,19 +1,16 @@
 function ViewModel () {
     var self = this;
 
-    self.labelText = ko.observable("Label text");
-    self.inputValue = ko.observable("Input value");
+    self.labelText = ko.observable("Nome:");
+    self.inputValue = ko.observable("");
     self.list = ko.observableArray([1,3,5,7,9,11]);
 
     self.buttonAction = function () {
         self.list.splice(0, 0, 100);
     }
 
-    self.inputValue.subscribe(function(novoValor) {
-        if(novoValor == "100")
-            self.labelText("Certo");
-        else
-            self.labelText("Errado")
+    self.computed = ko.computed(function () {
+        return `${self.labelText()} ${self.inputValue()}`
     })
 
 }
