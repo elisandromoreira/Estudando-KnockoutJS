ef8d9f4deda8f1a428f1f82f54570f436d34f65e
Elisandro Moreira <elisandro.moreira@univille.br>
2021-02-17 13:39:00 -0300

    Add observer subscriber

--- 02/app.js
+++ 02/app.js
@@ -3,11 +3,18 @@ function ViewModel () {
 
     self.labelText = ko.observable("Label text");
     self.inputValue = ko.observable("Input value");
-    self.list = ko.observableArray([1,3,5,7,9,11])
+    self.list = ko.observableArray([1,3,5,7,9,11]);
 
     self.buttonAction = function () {
-        console.log("clicked button");
+        self.list.splice(0, 0, 100);
     }
+
+    self.inputValue.subscribe(function(novoValor) {
+        if(novoValor == "100")
+            self.labelText("Certo");
+        else
+            self.labelText("Errado")
+    })
 
 }
 
